bbe30b88d4b33a92b5055a913aaa13bd56bf8171
pjpjpj <877302410@qq.com>
2017-08-01 10:42:36 +0800

    修改 游戏结束UI界面

--- gameOverScene.cpp
+++ gameOverScene.cpp
@@ -21,44 +21,56 @@ bool GameOverScene::init() {
     
     Size visible = Director::getInstance()->getWinSize();
     
-    auto bgLayer = LayerColor::create(Color4B(0, 0, 0, 255), visible.width, visible.height);
+    auto bgLayer = LayerColor::create(Color4B(30, 30, 30, 255), visible.width, visible.height);
     addChild(bgLayer);
     
-    auto contentLayer = Layer::create();
-    addChild(contentLayer);
-    contentLayer->setContentSize(Size(visible.width, visible.height / 2));
-    contentLayer->setPosition(Vec2(0, visible.height / 4));
+    // gamaOver缺个O
+    auto gameoverLogo = Sprite::create("res/gameover.png");
+    addChild(gameoverLogo);
+    gameoverLogo->setPosition(Vec2(visible.width / 2, visible.height * 0.8));
+    gameoverLogo->setScale(0.65, 0.65);
     
-    levelLabel = Label::createWithTTF("0", "fonts/arial.ttf", 128);
-    levelLabel->setPosition(Vec2(contentLayer->getContentSize().width / 2, contentLayer->getPosition().y + 128));
-    contentLayer->addChild(levelLabel);
+    // 补充 O
+    auto Ologo = Sprite::create("res/o.png");
+    addChild(Ologo);
+    Ologo->setScale(0.65, 0.65);
+    Ologo->setPosition(Vec2(visible.width / 2 + Ologo->getContentSize().width / 2 - 10, visible.height * 0.8));
+    ActionInterval * rotateto = RotateBy::create(0.5, 300);
+    Ologo->runAction(RepeatForever::create(Sequence::create(rotateto,NULL)));
     
-    auto bestLayer = Layer::create();
-    contentLayer->addChild(bestLayer);
-    bestLayer->setPosition(Vec2((visible.width - visible.width / 4) /2 , levelLabel->getPosition().y - 128 / 2 - 58));
-    bestLayer->setContentSize(Size(visible.width / 4, 58));
+    levelLabel = Label::createWithTTF("0", "fonts/arial.ttf", 200);
+    levelLabel->setPosition(Vec2(visible.width / 2, gameoverLogo->getPosition().y - gameoverLogo->getContentSize().height));
+    addChild(levelLabel);
     
-    bestLevelLabel = Label::createWithTTF("BEST", "fonts/arial.ttf", 38);
-    bestLevelLabel->setPosition(Vec2(bestLevelLabel->getContentSize().width / 1.5, bestLevelLabel->getContentSize().height / 2));
-    bestLayer->addChild(bestLevelLabel);
+    bestLevelLabel = Label::createWithTTF("BEST", "fonts/arial.ttf", 23);
+    bestLevelLabel->setPosition(Vec2(visible.width / 2 - 70, levelLabel->getPosition().y - levelLabel->getContentSize().height / 2));
+    addChild(bestLevelLabel);
     
-    auto bestLevelNumLable = Label::createWithTTF("0", "fonts/arial.ttf", 38);
-    bestLevelNumLable->setPosition(Vec2(bestLevelLabel->getPosition().x + bestLevelLabel->getContentSize().width / 2 + 38, bestLevelLabel->getPosition().y));
-    bestLayer->addChild(bestLevelNumLable);
+    auto bestLevelZHLabel = Label::create();
+    addChild(bestLevelZHLabel);
+    bestLevelZHLabel->setString("最佳分数");
+    bestLevelZHLabel->setSystemFontSize(25);
+    bestLevelZHLabel->setPosition(Vec2(visible.width / 2 - 75, bestLevelLabel->getPosition().y - bestLevelZHLabel->getContentSize().height / 2 - 25));
+    
+    auto bestLevelNumLable = Label::createWithTTF("0", "fonts/arial.ttf", 88);
+    bestLevelNumLable->setPosition(Vec2(visible.width / 2 + 50, bestLevelLabel->getPosition().y - bestLevelLabel->getContentSize().height / 1.5));
+    addChild(bestLevelNumLable);
     bestLevelNumLable->setString(UserDefault::getInstance()->getStringForKey("BestLevel"));
     
-    auto restartBtn = Button::create();
-    restartBtn->setTitleText("重新开始");
-    restartBtn->setTitleFontSize(48);
-    contentLayer->addChild(restartBtn);
-    restartBtn->setPosition(Vec2(contentLayer->getContentSize().width / 2, bestLayer->getPosition().y - 48 * 2));
+    auto restartBtn = Button::create("res/again.png");
+    restartBtn->setTitleText("再来一次 REPLAY");
+    restartBtn->setTitleFontSize(68);
+    restartBtn->setScale(0.7, 0.7);
+    addChild(restartBtn);
+    restartBtn->setPosition(Vec2(visible.width / 2, bestLevelNumLable->getPosition().y - restartBtn->getContentSize().height / 1.2));
     restartBtn->addTouchEventListener(CC_CALLBACK_2(GameOverScene::restartBtnClick, this));
     
-    auto backBtn = Button::create();
-    backBtn->setTitleText("回到菜单");
-    backBtn->setTitleFontSize(48);
-    contentLayer->addChild(backBtn);
-    backBtn->setPosition(Vec2(contentLayer->getContentSize().width / 2, restartBtn->getPosition().y - restartBtn->getContentSize().height - 48 / 2));
+    auto backBtn = Button::create("res/main.png");
+    backBtn->setTitleText("主菜单 MAIN MENU");
+    backBtn->setTitleFontSize(65);
+    backBtn->setScale(0.7, 0.7);
+    addChild(backBtn);
+    backBtn->setPosition(Vec2(visible.width / 2, restartBtn->getPosition().y - restartBtn->getContentSize().height));
     backBtn->addTouchEventListener(CC_CALLBACK_2(GameOverScene::backBtnClick, this));
     
     return true;
